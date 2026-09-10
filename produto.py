1bc3d680f4b05acd8663b64bb3a8c19715439e9b
Pedro <pedrooliveiradossantos2010@gmail.com>
2026-09-10 19:18:43 -0300

    Feat: Validação do cadastro do produto

--- produto.py
+++ produto.py
@@ -51,6 +51,36 @@ def cadastro_produto():
             break
 
         except ValueError:
-            print("Quantidade inválida! Digite um número inteiro.4")
+            print("Quantidade inválida! Digite um número inteiro.")
 
-    
+    try:
+        if os.path.exists(ARQUIVO):
+            with open(ARQUIVO, "r", encoding="utf-8") as arquivo:
+
+                for linha in arquivo:
+
+                    linha = linha.strip()
+
+                    if not linha:
+                        continue
+
+                    dados = linha.split(";")
+
+                    if len(dados) != 3:
+                        print("Existe uma linha inválida no arquivo.")
+                        return
+
+                    nome_cadastrado, preco_cadastrado, quantidade_cadastrada = dados
+
+                    if nome_cadastrado.lower() == nome.lower():
+                        print("Esse produto já está cadastrado.")
+                        return
+
+        with open(ARQUIVO, "a", encoding="utf-8") as arquivo:
+            arquivo.write(f"{nome};{preco:.2f};{quantidade}\n")
+
+        print("== PRODUTO CADASTRADO ==")
+        print("Produto cadastrado com sucesso!")
+
+    except OSError:
+        print("Não foi possível cadastrar o produto")
