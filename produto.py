834a8ea913309ececabccd21c149d7bdf5e9be28
Pedro <pedrooliveiradossantos2010@gmail.com>
2026-09-10 18:48:01 -0300

    Feat: Criando validação para nome do produto

--- produto.py
+++ produto.py
@@ -3,7 +3,18 @@ import os
 ARQUIVO = "produtos.txt"    
 
 def cadastro_produto():
+    print("\n --- CADASTRAR PRODUTO---")
 
     while True:
 
         nome = input("Nome do produto: ").strip()
+
+        if not nome:
+            print("Nome inválido. Tente novamente")
+            continue
+
+        if not nome.isalpha():
+            print("Nome inválido. Digite apenas letras.")
+            continue
+
+        break
