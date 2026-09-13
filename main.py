c7c7dbfb89a3c5f7c908949eaff457e0e0a30e73
Arthur Lira <liraarthur009@gmail.com>
2026-09-13 17:34:09 -0300

    feat: Adicionando a validação no menu

--- main.py
+++ main.py
@@ -2,45 +2,60 @@ from cliente import *
 from produto import *
 
 while True:
-    print(
-        Panel(
-            "[bright_gray]EMPRESA+[/]",
-            border_style = "deep_pink1",
-            width = 40,
-            padding = (0, 15)
+    try:
+        print(
+            Panel(
+                "[bright_gray]EMPRESA+[/]",
+                border_style = "deep_pink1",
+                width = 40,
+                padding = (0, 15)
+            
+            ))
         
-        ))
-    
-    print()
-    print()
-    
-    print(Panel(
-"""
-[light_goldenrod2][ 1 ] Menu Produtos[/]
+        print()
+        
+        print(Panel(
+    """
+    [light_goldenrod2][ 1 ] Menu Produtos[/]
 
-[royal_blue1][ 2 ] Menu Cliente[/]
-""",
-            box = box.ROUNDED,
-            border_style= "deep_pink1",
-            padding= (1, 8),
-            width = 40,
-            title = "[bright_white]CADASTRO[/]",
-            subtitle = "[deep_pink1]Pressione 3 para sair[/]",
-            subtitle_align= "right",
+    [royal_blue1][ 2 ] Menu Cliente[/]
+    """,
+                box = box.ROUNDED,
+                border_style= "deep_pink1",
+                padding= (1, 5),
+                width = 40,
+                title = "[bright_white]CADASTRO[/]",
+                subtitle = "[deep_pink1]Pressione 3 para sair[/]",
+                subtitle_align= "right",
 
-))
+    ))
 
-    resp = int(input(" "))
+        resp = int(input(" "))
 
 
-    match resp:
-        case 1:
-            pass
-        case 2:
-            menu_cliente()
+        match resp:
+            case 1:
+                pass
+            case 2:
+                print()
+                menu_cliente()
+                print()
 
-        case 3:
-            break
+            case 3:
+                break
 
-        case _:
-            raise ValueError
+            case _:
+                raise ValueError
+            
+    except ValueError:
+        print()
+        print(Panel(
+            "[bright_red]Opção Inválida![/]", 
+            box=box.ROUNDED, 
+            expand = False, 
+            width = 80,
+            border_style="red",
+            )
+        )
+        print()
+        print()
