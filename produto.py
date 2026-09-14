406c16f1eaf2f59feedb634d1946a2ea0b1921a5
Murilo <nseitbmnsei6@gmail.com>
2026-09-13 22:09:22 -0300

    Style: Adicionando as estilizações do rich

--- produto.py
+++ produto.py
@@ -1,20 +1,25 @@
 import os
 from colorama import Fore, Style, init
+from rich import print
+from rich.panel import Panel
+from rich import box
+from rich.table import Table
 
 init(autoreset=True)
-
-def caixa(texto, cor=Fore.CYAN, largura=50):
-    linhas = texto.split("\n")
-    print(cor + "┌" + "─" * (largura - 2) + "┐")
-    for linha in linhas:
-        print(cor + "│" + Style.BRIGHT + linha.center(largura - 2) + Style.NORMAL + cor + "│")
-    print(cor + "└" + "─" * (largura - 2) + "┘")
-
 
 txt = "produtos.txt"    
 
 def cadastro_produto():
-    caixa("CADASTRAR PRODUTO", Fore.CYAN)
+
+    print(Panel(
+        "Cadastro de Produto", 
+        box= box.DOUBLE, 
+        width= 50, 
+        padding= (0, 14), 
+        border_style="green3")
+        )
+
+    print()
 
     while True:
         try:
@@ -27,8 +32,19 @@ def cadastro_produto():
                 raise ValueError
             
             break
+
         except ValueError:
-            print(Fore.RED + "Nome inválido! Digite apenas letras.")
+            print()
+            print(Panel(
+                "[bright_red]Nome Inválido![/]",
+                box=box.ROUNDED,
+                expand= False,
+                width=80,
+                border_style="red",
+            ))
+            print()
+            print()
+    print()
 
     while True:
 
@@ -41,7 +57,17 @@ def cadastro_produto():
             break
 
         except ValueError:
-            print(Fore.RED + "Preço inválido! Digite somente números.")
+            print()
+            print(Panel(
+                "[bright_red]Preço Inválido![/]",
+                box=box.ROUNDED,
+                expand= False,
+                width=80,
+                border_style="red",
+            ))
+            print()
+            print()
+    print()
 
 
     while True:
@@ -55,7 +81,17 @@ def cadastro_produto():
             break
 
         except ValueError:
-            print(Fore.RED + "Quantidade inválida! Digite um número inteiro.")
+            print()
+            print(Panel(
+                "[bright_red]Quantidade Inválido![/]",
+                box=box.ROUNDED,
+                expand= False,
+                width=80,
+                border_style="red",
+            ))
+            print()
+            print()
+    print()
 
     try:
         if os.path.exists(txt):
@@ -73,7 +109,7 @@ def cadastro_produto():
                     if len(dados) != 3:
                         raise FileExistsError
 
-                    nome_cadastrado, _, _ = dados
+                    nome_cadastrado, _ , _ = dados
 
                     if nome_cadastrado.lower() == nome.lower():
                         raise ValueError
@@ -81,119 +117,261 @@ def cadastro_produto():
         with open(txt, "a", encoding="utf-8") as arquivo:
             arquivo.write(f"{nome};{preco:.2f};{quantidade}\n")
 
-        caixa("PRODUTO CADASTRADO", Fore.GREEN)
-        print(Fore.YELLOW + f"Nome: {Style.BRIGHT}{nome}")
-        print(Fore.YELLOW + f"Preço: {Style.BRIGHT}R$ {preco:.2f}")
-        print(Fore.YELLOW + f"Quantidade: {Style.BRIGHT}{quantidade}")
-        print(Fore.GREEN + "Produto cadastrado com sucesso!")
+        print(Panel(
+            "Produto cadastrado com sucesso", 
+            box= box.DOUBLE, 
+            width= 50, 
+            padding= (0, 8), 
+            border_style="bright_green")
+        )
+
+        print()
+        print(f"[orange3]Nome:[/] {nome}")
+        print()
+        print(f"[orange3]Preço:[/] R$ {preco:.2f}")
+        print()
+        print(f"[orange3]Quantidade:[/] {quantidade}")
+        print()
 
     except OSError:
-        print("Não foi possível cadastrar o produto")
-
+        print()
+        print(Panel(
+            "[bright_red]Não foi possivel cadastrar o produto![/]",
+            box=box.ROUNDED,
+            expand= False,
+            border_style="red",
+        ))
+        print()
+        print()
+    print()
 
 def listar_produto():
-    caixa("PRODUTOS CADASTRADOS", Fore.CYAN)
-
-    if not os.path.exists(txt):
-        print(Fore.RED + "Nenhum produto cadastrado.")
-        return
-
     try:
-        produtos = []
+        if os.path.exists(txt):
+            with open(txt, "r", encoding = "utf-8") as arquivo:
+                produto = arquivo.readlines()
 
-        with open(txt, "r", encoding="utf-8") as arquivo:
+            if produto:   
+                produto.sort()
 
-            for linha in arquivo:
+                print(Panel(
+                    "Listar Produto", 
+                    box= box.DOUBLE, 
+                    width= 80, 
+                    padding= (0, 32), 
+                    border_style="dodger_blue3")
+                 )
 
-                linha = linha.strip()
+                print()
 
-                if not linha:
-                    raise FileNotFoundError
 
-                dados = linha.split(";")
-
-                if len(dados) != 3:
-                    raise ValueError
-
-                nome, preco, quantidade = dados
-
-                preco = float(preco)
-                quantidade = int(quantidade)
-
-                produtos.append((nome, preco, quantidade))
-
-            if not produtos:
-                raise FileNotFoundError
-            
-            print(Fore.BLUE + Style.BRIGHT + f"{'NOME':<25} {'PREÇO':<12} {'QUANTIDADE':<12}")
-            print(Fore.CYAN + "-" * 48)
-
-            for nome, preco, quantidade in produtos:
-                print(
-                    Fore.WHITE + Style.BRIGHT + f"{nome:<25}"
-                    + Fore.YELLOW + f"R$ {preco:<9.2f}"
-                    + Fore.YELLOW + f"{quantidade:<12}"
+                tabela = Table(
+                    caption = "EMPRESA+",
+                    caption_style= "dim italic #000a9b",
+                    title_style = "bold white on dark_blue",
+                    border_style="bright_blue",
+                    box = box.ROUNDED,
+                    style = "cyan",
+                    width = 80,
+                    show_lines= True
                 )
 
-    except (OSError, ValueError, FileNotFoundError):
-        print(Fore.RED + "Não foi possível ler os produtos cadastrados.")
+                tabela.add_column("NOME", justify = "center", style="bold white")
+                tabela.add_column("PREÇO", justify = "center", style="italic magenta")
+                tabela.add_column("QUANTIDADE", justify = "center", style="yellow")
+
+                for prod in produto:
+                    dados = prod.strip().split(";")
+                    nome, preco, qtd = dados
+
+                    tabela.add_row(nome, preco, qtd)
+
+                print(tabela)
+                print()
+
+            else:
+                raise FileNotFoundError
+
+        else:
+            raise FileNotFoundError
+            
+    except FileNotFoundError:
+        print(Panel(
+            "[bright_red]Nenhum Produto Cadastrado[/]",
+            expand = False,
+            title_align= "center",
+            border_style= "red",
+            width = 80
+        ))
 
 def excluir_produto():
 
     while True:
         try:
-            caixa("EXCLUIR PRODUTO", Fore.CYAN)
+            global txt
+            if os.path.exists(txt):
+                with open(txt, "r", encoding = "utf-8") as arquivo:
+                    produtos = arquivo.readlines()
 
-            if not os.path.exists(txt):
-                raise FileNotFoundError
+            if produtos:
+                produtos.sort()
 
-            nome_excluir = input(Fore.MAGENTA + Style.BRIGHT + "\nDigite o nome do produto que deseja excluir: ").strip().title()
+                print(Panel(
+                    "Exclusão de Produtos", 
+                    box= box.DOUBLE, 
+                    width= 80, 
+                    padding= (0, 28), 
+                    border_style="bright_red")
+                )
 
-            if not nome_excluir:
-                raise ValueError
+                print()
 
-            elif not nome_excluir.replace(" ", "").isalpha():
-                raise ValueError
 
-       
-            produtos = []
-            encontrado = False
+                tabela = Table(
+                        caption = "EMPRESA+",
+                        caption_style= "dim italic #000a9b",
+                        title_style = "bold white on dark_red",
+                        border_style="bright_red",
+                        box = box.ROUNDED,
+                        style = "bright_red",
+                        width = 80,
+                        show_lines= True
+                    )
 
-            with open(txt, "r", encoding="utf-8") as arquivo:
-
-                for linha in arquivo:
-
-                    linha = linha.strip()
-
-                    if not linha:
-                        raise FileNotFoundError
-
-                    dados = linha.split(";")
-
-                    if len(dados) != 3:
-                        raise ValueError
-
-                    nome, _, _ = dados
-
-                    if nome.lower() == nome_excluir.lower():
-                        encontrado = True
-                        continue
-
-                    produtos.append(linha)
-
-            if not encontrado:
-                raise ValueError
-
-            with open(txt, "w", encoding="utf-8") as arquivo:
+                tabela.add_column("NOME", justify = "center", style="bold white")
+                tabela.add_column("PREÇO", justify = "center", style="italic magenta")
+                tabela.add_column("QUANTIDADE", justify = "center", style="yellow")
 
                 for produto in produtos:
-                    arquivo.write(produto + "\n")
+                    dados = produto.strip().split(";")
+                    nome, preco, qtd = dados
 
-            caixa("PRODUTO EXCLUÍDO", Fore.GREEN)
-            print(Fore.GREEN + "Produto excluído com sucesso!")
+                    tabela.add_row(nome, preco, qtd)
 
+                print(tabela)
+                print()
+
+            else:
+                raise FileNotFoundError
+
+            
+            dados = []
+            with open(txt, "r", encoding= "utf-8") as arquivo:
+                dados = arquivo.readlines()
+
+            while True:
+                try:
+                    nome = input("Digite o nome do produto para excluí-lo: ").strip().title()
+
+                    if not nome.replace(" ", "").isalpha():
+
+                        raise NameError
+
+                    else:
+                        for n in dados:
+                            if n.startswith(nome + ";"):
+                                break
+
+                        else:
+                            raise NameError
+
+                        break
+
+                except NameError:
+                    print(Panel(
+                        "[bright_red]Produto não encontrado[/]",
+                        expand = False,
+                        title_align= "center",
+                        border_style= "red",
+                        width = 80
+                        ))
+
+            with open(txt, "w", encoding = "utf-8") as arquivo:
+                for n in dados:
+                    if not n.startswith(nome + ";"):
+                        arquivo.write(n)
+
+            print()
+            print()
+            print(
+                Panel(
+                    "[bright_green]Produto Excluido com Sucesso[/]", 
+                    expand= False,
+                    title_align="center",
+                    border_style="green3"
+                )
+            )
+            break        
+        except FileNotFoundError:
+            print(Panel(
+                "[bright_red]Nenhum Produto Cadastrado[/]",
+                expand = False,
+                title_align= "center",
+                border_style= "red",
+                width = 80
+            ))
             break
+        
+def menu_cliente():
+    while True:
+        try:
+            print(
+                Panel(
+                    "[bright_gray]PRODUTO[/]",
+                    border_style = "gold3",
+                    width = 40,
+                    padding = (0, 15)
+                ))
+            print()
+            
+            print(Panel(
+    """
+[chartreuse1][ 1 ] Cadastrar Produto[/]
 
-        except (OSError, FileNotFoundError, ValueError):
-            print("Erro ao excluir produto!")
+[royal_blue1][ 2 ] Listar Produto[/]
 
+[indian_red1][ 3 ] Excluir Produto[/]
+    """,
+                box = box.ROUNDED,
+                border_style= "gold3",
+                padding= (1, 6),
+                width = 40,
+                title = "[bright_white]CADASTRO[/]",
+                subtitle = "[turquese1]Pressione 4 para sair[/]",
+                subtitle_align= "right",
+
+        ))
+
+            resp = int(input())
+
+            match resp:
+                case 1:
+                    cadastro_produto()
+
+                case 2:
+                    listar_produto()
+
+                case 3:
+                    excluir_produto()
+
+                case 4:
+                    break
+
+                case _:
+                    raise ValueError
+                
+        except ValueError:
+            print()
+            print(Panel(
+                "[bright_red]Opção Inválida![/]", 
+                box=box.ROUNDED, 
+                expand = False, 
+                width = 80,
+                border_style="red",
+                )
+            )
+            print()
+            print()
+
+        
+menu_cliente()
